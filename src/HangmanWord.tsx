16986d32f0fcc85f1f6b0bc2caec3c4c1d1befdd
Carlos Vanzego <129208650+CarlosVanzego@users.noreply.github.com>
2024-01-30 00:49:15 -0500

    Update HangmanWord.tsx

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,36 +1,38 @@
+// Component props definition
 type HangmanWordProps = {
-  guessedLetters: string[]
-    wordToGuess: string 
-    reveal?: boolean
+  guessedLetters: string[] // Array of guessed letters
+  wordToGuess: string // Word to be guessed
+  reveal?: boolean // Flag to reveal the entire word
 }
 
-export function HangmanWord({ guessedLetters, wordToGuess, reveal= false }: HangmanWordProps) {
+// HangmanWord component definition
+export function HangmanWord({ guessedLetters, wordToGuess, reveal = false }: HangmanWordProps) {
   return (
     <div 
-    style={{ 
-      display: "flex", 
-      gap: ".25em", 
-      fontSize: "6rem", 
-      fontWeight: "bold", 
-      textTransform: "uppercase", 
-      fontFamily: "monospace"
-    }}
-  >
-    {wordToGuess.split("").map((letter, index) => (
-      <span style={{ borderBottom: ".1em solid black" }} key={index}>
-        <span style={{
-          visibility: guessedLetters.includes(letter) || reveal
-          ? "visible"
-          : "hidden",
-          color: 
-          !guessedLetters.includes(letter) && reveal ? "red" : "black"
-        }}
-      >
-         {letter}
+      style={{ 
+        display: "flex", 
+        gap: ".25em", 
+        fontSize: "6rem", 
+        fontWeight: "bold", 
+        textTransform: "uppercase", 
+        fontFamily: "monospace"
+      }}
+    >
+      {/* Render each letter of the word */}
+      {wordToGuess.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          {/* Render each letter with appropriate styling */}
+          <span style={{
+            // Set visibility based on whether the letter is guessed or revealed
+            visibility: guessedLetters.includes(letter) || reveal ? "visible" : "hidden",
+            // Set color based on whether the letter is not guessed and revealed
+            color: !guessedLetters.includes(letter) && reveal ? "red" : "black"
+          }}>
+            {/* Render the letter */}
+            {letter}
+          </span>
         </span>
-      </span>
-    ))}
-  </div>
+      ))}
+    </div>
   )
 }
-
