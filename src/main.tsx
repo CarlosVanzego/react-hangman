9d297d70d9128e8410fdc48e7a3e02af192ec683
Carlos Vanzego <129208650+CarlosVanzego@users.noreply.github.com>
2024-01-30 00:50:24 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// Importing React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Importing the main component of the application
+import App from './App.tsx';
 
+// Rendering the main component wrapped in React.StrictMode
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
